e72a665368a6414bbca2fa5a9dd93991307a7caf
Chris <chriscar.sam@gmail.com>
2021-02-03 12:23:08 -0500

    Mostrar los titulos de las imágenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import GifGridItem from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState(0);
-
+    const [images, setImages] = useState([]);
+    
     useEffect( () => {
         getGifs();
     }, [])
@@ -23,16 +24,24 @@ export const GifGrid = ({ category }) => {
         })
 
         console.log(gifs);
-
+        setImages( gifs );
     }
 
     // getGifs();
 
     return (
         <div>
-            <h3>{ category }</h3>
-            <h3>{ count }</h3>
-            <button onClick={ ()=> setCount( count + 1) }></button>
+            <h3>{ category }</h3>          
+             
+                 {
+                     images.map( img => (
+                        <GifGridItem 
+                            key={ img.id }
+                            { ...img }
+                        />
+                     ))
+                 }
+             
         </div>
     )
 }
