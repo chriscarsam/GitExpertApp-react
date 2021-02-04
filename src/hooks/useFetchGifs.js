d78647e58a20de9837220cc1fe761cc29bd703a3
Chris <chriscar.sam@gmail.com>
2021-02-04 17:56:31 -0500

    Animaciones por CSS en nuestra aplicación

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -20,7 +20,7 @@ export const useFetchGifs = ( category ) => {
                         loading: false
                     });
 
-                },3000 );
+                },2000 );
             })
                
     }, [category])
